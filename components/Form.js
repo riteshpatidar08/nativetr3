b5b9fc3e605b2fc3701e4a3f75d770ab26fa8205
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-10-05 14:08:02 +0530

    form added

--- components/Form.js
+++ components/Form.js
@@ -1,11 +1,18 @@
-import { TextInput, View } from "react-native"
+import { Button, TextInput, View } from "react-native"
 export default function Form(){
     return (
-<View style={{flex:1 , justifyContent:'center' }}>
+<View style={{flex:1 , justifyContent:'center' ,margin:10}}>
     <TextInput
-    style={{height:40 , borderColor:'grey'}}
+    style={{height:40 , borderColor:'gray' ,borderWidth:1 ,margin:5, padding:8}}
     placeholder ="Email"
+    keyboardType = 'email-address'
     />
+    <TextInput
+    style={{height:40 , borderColor:'gray' ,borderWidth:1, padding:8 , margin:5}}
+    placeholder ="Password"
+   secureTextEntry
+    />
+    <Button style={{marginTop:10}} title="Login"/>
 </View>
     )
 }
