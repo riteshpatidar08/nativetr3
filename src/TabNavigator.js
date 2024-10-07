0f5707fe5eb40fd4a11e7848e13d971ada7cfcdb
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-10-07 13:57:32 +0530

    updated till image picker

--- src/TabNavigator.js
+++ src/TabNavigator.js
@@ -1,41 +1,40 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import HomeScreen from "./screens/HomeScreen";
-import React from 'react'
-import ProfileScreen from "./screens/ProfileScreen";
-import SearchScreen from "./screens/SearchScreen";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen';
+import React from 'react';
+import ProfileScreen from './screens/ProfileScreen';
+import SearchScreen from './screens/SearchScreen';
 import Icons from 'react-native-vector-icons/Ionicons';
 function TabNavigator() {
-    const Tab = createBottomTabNavigator() ;
+  const Tab = createBottomTabNavigator();
   return (
-   <Tab.Navigator   screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'home'
-                : 'home-outline';
-            } else if (route.name === 'Profile') {
-              iconName = focused ? 'person' : 'person-outline';
-            }else if(route.name ==='Search'){
-                iconName = focused ? 'search' : 'search-outline'
-            }
-
-            // You can return any component that you like here!
-            return <Icons name={iconName} size={20} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-          tabBarLabelStyle:{
-            margin: 10
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'person' : 'person-outline';
+          } else if (route.name === 'Search') {
+            iconName = focused ? 'search' : 'search-outline';
           }
-        })}>
-    <Tab.Screen name="Home" component={HomeScreen}/>
-    <Tab.Screen name="Profile"component={ProfileScreen}/>
-    <Tab.Screen name="Search" component={SearchScreen}/>
 
-   </Tab.Navigator>
-  )
+         
+          return <Icons name={iconName} size={20} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+        tabBarLabelStyle: {
+          margin: 5,
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Search" component={SearchScreen} />
+    </Tab.Navigator>
+  );
 }
 
-export default TabNavigator
+export default TabNavigator;
